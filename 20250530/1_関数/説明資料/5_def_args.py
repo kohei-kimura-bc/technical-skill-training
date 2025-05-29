1452cf6bc8d87a6f433c97cc90bf5ad219176c1c
koheikimura <kohei-kimura@mail.nissan.co.jp>
2025-05-29 13:52:53 +0900

    argsの説明資料を微修正

--- 20250530/1_関数/説明資料/5_def_args.py
+++ 20250530/1_関数/説明資料/5_def_args.py
@@ -1,6 +1,9 @@
 def sum_func(*args):
-    print(*args)  # (1,2,3,4,5)
-    return sum(args)  # sum(1,2,3,4,5)
+    print(args)  # (1,2,3,4,5)
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
 
 
 total = sum_func(1, 2, 3, 4, 5)
